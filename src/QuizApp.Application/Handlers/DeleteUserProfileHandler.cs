0a8a7f5c91ebb740f85547ee4db7d7ddf19dc6fd
Serhii Dmytrenko <dmmytrenko@gmail.com>
2025-02-10 09:20:00 +0200

    user profile delete endpoint implemented

---
+++ src/QuizApp.Application/Handlers/DeleteUserProfileHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using QuizApp.Contracts.Rest.Models;
+using QuizApp.Contracts.Rest.Requests;
+using QuizApp.Domain.Exceptions;
+using QuizApp.Domain.Models;
+using System.Net;
+
+namespace QuizApp.Application.Handlers;
+
+public class DeleteUserProfileHandler(UserManager<User> userManager) : IRequestHandler<DeleteUserProfileRequest>
+{
+    public async Task Handle(DeleteUserProfileRequest request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByIdAsync(request.UserId);
+        if (user == null)
+        {
+            throw new DomainException("User not found.");
+        }
+
+        var deleteResult = await userManager.DeleteAsync(user);
+
+        if (!deleteResult.Succeeded)
+        {
+            throw new DomainException("Failed to delete user.", (int)HttpStatusCode.BadRequest, deleteResult.Errors.ToDictionary(x => x.Code, x => x.Description));
+        }
+    }
+}
