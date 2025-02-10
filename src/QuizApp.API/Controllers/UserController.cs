1a3f54640de1a22a505f9a8c6c15ed6494ebd5bd
Serhii Dmytrenko <dmmytrenko@gmail.com>
2025-02-10 08:33:52 +0200

    user profile updated

--- src/QuizApp.API/Controllers/UserController.cs
+++ src/QuizApp.API/Controllers/UserController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using QuizApp.Contracts.Rest.Models;
 using QuizApp.Contracts.Rest.Requests;
 using QuizApp.Contracts.Rest.Responses;
 using System.Security.Claims;
@@ -12,21 +11,22 @@ namespace QuizApp.API.Controllers;
 [Route("api/[controller]")]
 public class UserController(IMediator mediator) : ControllerBase
 {
-    [Authorize]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [HttpPost("user-profile/update")]
     [Consumes("multipart/form-data")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public async Task<UpdateUserProfileResponse> UpdateUserProfile([FromForm] UpdateUserProfileRequest request)
     {
+        request.UserId ??= User.FindFirstValue(ClaimTypes.NameIdentifier);
+
         var updatedResult = await mediator.Send(request);
         return updatedResult;
     }
 
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [HttpGet("user-profile")]
-    public async Task<GetUserProfileResponse> GetUserProfile()
+    public async Task<GetUserProfileResponse> GetUserProfile([FromQuery] string? userId)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        userId ??= User.FindFirstValue(ClaimTypes.NameIdentifier);
 
         var getUserProfileRequest = new GetUserProfileRequest(userId);
         var userProfile = await mediator.Send(getUserProfileRequest);
